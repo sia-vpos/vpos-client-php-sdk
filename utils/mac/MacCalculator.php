758168726ca57f140b6e7bdf4914afcb32acb46a
Gabriel Raul Marini <gab.marini@reply.it>
2020-01-20 14:43:49 +0100

    Implemented MAC calculator with relative JAVADOC

---
+++ utils/mac/MacCalculator.php
@@ -0,0 +1,35 @@
+<?php
+
+
+/**
+ * Class MacCalculator
+ *
+ * Perform simple HMAC calculations to support integrity check
+ * with mutual authentication
+ *
+ * @author Gabriel Raul Marini
+ */
+class MacCalculator
+{
+    private string $algorithm;
+
+    /**
+     * MacCalculator constructor.
+     * @param int $algorithmIndex use AlgorithmEnum to specify the hash algorithm
+     */
+    public function __construct(int $algorithmIndex)
+    {
+        $algorithmArray = hash_hmac_algos();
+        $this->algorithm = $algorithmArray[$algorithmIndex];
+    }
+
+    /**
+     * @param string $value on which MAC is calculated
+     * @param string $key used to encode the string
+     * @return string the MAC in HEX format
+     */
+    public function calculate(string $value, string $key)
+    {
+        return hash_hmac($this->algorithm, $value, $key);
+    }
+}
