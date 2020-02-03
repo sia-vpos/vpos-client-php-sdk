f58680bfc4d41ef66421f369b9e5e8a7e306129b
Gabriel Raul Marini <gab.marini@reply.it>
2020-02-03 13:01:29 +0100

    Implemented test for Auth3Ds request

---
+++ TestAuth3DS.php
@@ -0,0 +1,34 @@
+<?php
+require("utils/Utils.php");
+require("dto/request/Auth3DSDto.php");
+require("client/VPOSClient.php");
+
+const SHOP_ID = "129281292800109";
+const PAN_ALIAS = "0000298241731500671";
+const API_RESULT_KEY = "E-vmE-GHXmx73-Lfg24LztZ-7-yCyVsKn4QXphL5qzf-Kr-Cf-JWpZwZgaZRA5dR9V677xL4uCbc-Ce--8h2-tdrSu--QKjF-nZh";
+const MAC_KEY_REDIRECT = "fU-9et-s-Sj8W---E8uhUDu9fEzqr8hH3L95s48r9nq-cq3cBXbp-tZsvGQU--t-nqmtaW-7x-7-C2PdcuFdbHuShQ-pYVWnr-4-";
+const URL_WEB_API = "https://atpostest.ssb.it/atpos/apibo/apiBOXML.app";
+
+
+$vPOSClient = new VPOSClient(MAC_KEY_REDIRECT, API_RESULT_KEY, URL_WEB_API);
+$response = $vPOSClient->startAuth3DS(buildAuth3DSTest1());
+var_dump($response);
+
+
+function buildAuth3DSTest1()
+{
+    $dto = new Auth3DSDto();
+    $dto->setIsMasterpass(false);
+    $dto->setOrderId(Utils::generateRandomDigits());
+    $dto->setPan(PAN_ALIAS);
+    $dto->setExpDate("2112");
+    $dto->setAmount("2000");
+    $dto->setCurrency("978");
+    $dto->setCvv2("111");
+    $dto->setAccountingMode("D");
+    $dto->setNetwork("98");
+    $dto->setShopId(SHOP_ID);
+    $dto->setOperatorId("operator");
+    return $dto;
+}
+
