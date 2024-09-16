79d43dfab4a5964c0d90dd46de91a1a8be4a0767
sia-vpos <68986946+sia-vpos@users.noreply.github.com>
2024-09-16 17:42:07 +0200

    Update Test.php
    updated the test class on the 3ds2.0 framework

--- Test.php
+++ Test.php
@@ -1,78 +1,48 @@
 <?php
-require("utils/mac/MacCalculator.php");
-require("utils/mac/AlgorithmEnum.php");
-require("utils/HTMLGenerator.php");
-require("utils/encryption/AESEncoder.php");
-require("utils/Utils.php");
-require("utils/apos/RestClient.php");
-require("client/VPOSClient.php");
+require_once("client/ClientConfig.php");
+require_once("client/VPOSClient.php");
+require_once("models/request/Auth3DS2Step0.php");
 
+const SHOP_ID = "129281292800040";
+const REDIRECT_KEY = "";
+const API_KEY = "N-qVfy-EvuNShpareCH-CR-RrG7gdnM5XbbLeXA395uVkx-jgS--7AHSvC9aVVvjt8XbNL6-9BxjA-FdSpuMCp-XbLNNqYmuZ-Xc";
 
+const URL_API = "https://atpostest.ssb.it/atpos/apibo/apiBOXML.app";
+const URL_REDIRECT = "https://atpostest.ssb.it/atpos/pagamenti/main";
 
-$macCalculator = new MacCalculator(AlgorithmEnum::SHA_256);
-$htmlGenerator = new HTMLGenerator();
+date_default_timezone_set('Europe/Rome');
+$config = new ClientConfig(SHOP_ID, REDIRECT_KEY, API_KEY, URL_API, URL_REDIRECT);
+$vPOSClient = new VPOSClient($config);
+$response = $vPOSClient->start3DS2Step0(buildAuth3DSStep0());
+var_dump($response);
 
-//echo($macCalculator->calculate("URLMS=https://te.t-frutta.eu/TImooneyWS/app_api/v10/payment/cardData?consumerId=3b350c34-d923-4552-91bf-67bc4f99da92&URLDONE=http://localhost:8080/payment-gateway/vpos/tokenize&ORDERID=s564c564c6as54as654c65as4&SHOPID=129281292800104&AMOUNT=10&CURRENCY=978&ACCOUNTINGMODE=D&AUTHORMODE=I&OPTIONS=GM", "CNCuDT5Vyws2v9t--RhDdtCwstaV2tqVeqfE-D8G-S-Ds--C-fFkSBxw-3wWBqBC--U9hwN-H-Mj4ZZHMr--YSHLdU5WKLx-cT-T"));
-//echo($htmlGenerator->generateParamsHtml(array("ORDERID" => "45646", "OPERATORID" => "mkdsm")));
-//echo ($htmlGenerator->base64ToHtml("PGh0bWw+PGJvZHk+PC9ib2R5PjwvaHRtbD4K", 500));
-//echo $htmlGenerator->htmlToBase64("null", array("ORDERID"=>"12345678"));
-echo AESEncoder::encrypt("aaaaaaaaaaaaaaaa", "ciao");
-//echo Utils::generateRandomDigits();
-//echo $macCalculator->calculate("OPERATION=AUTHORIZATION3DSSTEP1&TIMESTAMP=2020-01-27T15:01:56.000&SHOPID=129281292800109&ORDERID=413999151994&OPERATORID=operator&REQREFNUM=20200127471727070197783123084621&PAN=0000409500729966732&CVV2=111&EXPDATE=2112&AMOUNT=2000&CURRENCY=978&ACCOUNTINGMODE=D&NETWORK=98", "E-vmE-GHXmx73-Lfg24LztZ-7-yCyVsKn4QXphL5qzf-Kr-Cf-JWpZwZgaZRA5dR9V677xL4uCbc-Ce--8h2-tdrSu--QKjF-nZh");
+function buildAuth3DSStep0()
+{
+    $auth3DS = new Auth3DS2Step0();
+// with pan   
+//    $auth3DS -> setPan("4118830900940017");
+//    $auth3DS -> setNetwork("01");
+// with token
+    $auth3DS -> setPan("0000423178147851547");
+    $auth3DS -> setNetwork("98");
+// end with    
+    $auth3DS -> setOperatorId("testopid");
+    $auth3DS -> setAmount("2000");
+    $auth3DS -> setAccountingMode("D");
+    $auth3DS -> setExpDate("2512");
+    $auth3DS -> setCvv2("111");
+    $auth3DS -> setCurrency("978");
+    $auth3DS -> setEmailCh("tester@gmail.com");
+    $auth3DS -> setExponent("2");
+    $auth3DS -> setNameCH("mario testa");
+    $auth3DS -> setOrderId(substr(Utils::generateRandomDigits(), 0, 12));
+    $auth3DS -> setTRecurr("C");
+    $auth3DS -> setCRecurr("COF0123456789");
+    $auth3DS -> setUserId("testusrid");
+    $auth3DS -> setProductRef("testref");
+    $auth3DS -> setNotifURL("https://atpostest.ssb.it/atpos/apibo/en/3ds-notification.html");
+    $auth3DS -> setThreeDSMtdNotifURL("https://atpostest.ssb.it/atpos/apibo/en/3ds-notification.html");
+    $auth3DS -> setThreeDSData('{"addrMatch":"N","chAccAgeInd":"04","chAccChange":"20190211","chAccChangeInd":"03","chAccDate":"20190210","chAccPwChange":"20190214","chAccPwChangeInd":"04","nbPurchaseAccount":"1000","txnActivityDay":"100","txnActivityYear":"100","shipAddressUsage":"20181220","shipAddressUsageInd":"03","shipNameIndicator":"01","billAddrCity":"billAddrCity","billAddrCountry":"004","billAddrLine1":"billAddrLine1","billAddrLine2":"billAddrLine2","billAddrLine3":"billAddrLine3","billAddrPostCode":"billAddrPostCode","billAddrState":"11","homePhone":"039-321818198111","mobilePhone":"33-312","shipAddrCity":"zio","shipAddrCountry":"008","shipAddrLine1":"shipAddrLine1","shipAddrLine2":"shipAddrLine2","shipAddrLine3":"shipAddrLine3","shipAddrPostCode":"shipAddrPostCode","shipAddrState":"222","workPhone":"39-0321818198","deliveryEmailAddress":"a-b@example.com","deliveryTimeframe":"02","preOrderDate":"20181220","preOrderPurchaseInd":"01","reorderItemsInd":"02","shipIndicator":"01","browserAcceptHeader":"text/html,application/xhtml+xml,application/xml;","browserIP":"10.42.195.152","browserJavaEnabled":"true","browserLanguage":"it-IT","browserColorDepth":"16","browserScreenHeight":"1024","browserScreenWidth":"1920","browserTZ":"-120","browserUserAgent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}');
 
-$restClient = new RestClient();
-testGetXMLAuth3DS();
-
-$dataJson = new Data3DSJson();
-$dataJson->setAcctID("fdsfkjds");
-$dataJson->setAddrMatch("sfdsad");
-
-//echo $dataJson->__toString();
-////testGetXMLAuth3DSStep2();
-
-
-function testGetXMLAuth3DS():void{
-    $auth3DS = new Auth3DSDto();
-    $vposClient = new VPOSClient("E-vmE-GHXmx73-Lfg24LztZ-7-yCyVsKn4QXphL5qzf-Kr-Cf-JWpZwZgaZRA5dR9V677xL4uCbc-Ce--8h2-tdrSu--QKjF-nZh", "https://atpostest.ssb.it/atpos/apibo/apiBOXML.app");
-
-    $auth3DS->setIsMasterpass(false);
-    $auth3DS->setShopId("129281292800109");
-    $auth3DS->setOperatorId("operator");
-
-//    $auth3DS->setService("service");
-//    $auth3DS->setEci("eci");
-//    $auth3DS->setXId("xid");
-//    $auth3DS->setCavv("cavv");
-//    $auth3DS->setParesStatus("paresStatus");
-//    $auth3DS->setScenRollStatus("scEnrollStatus");
-//    $auth3DS->setSignatureVerification("signatureVerifytion");
-//    $auth3DS->setPpAuthenticateMethod("ppAuthenticationMode");
-//    $auth3DS->setCardEnrollMethod("ppACardEnrollMethod");
-    $auth3DS->setOrderId(substr(Utils::generateRandomDigits(), 0, 12));
-    $auth3DS->setPan("0000409500729966732");
-    $auth3DS->setCvv2("111");
-    $auth3DS->setExpDate("2112");
-    $auth3DS->setAmount("2000");
-    $auth3DS->setCurrency("978");
-//    $auth3DS->setExponent("exponent");
-    $auth3DS->setAccountingMode("D");
-    $auth3DS->setNetwork("98");
-//   $auth3DS->setEmailCh("email@libero.it");
-//    $auth3DS->setUserId("userId");
-//    $auth3DS->setProductRef("productRef");
-//    $auth3DS->setName("name");
-//    $auth3DS->setSurname("surname");
-
-    $vposClient->startAuth3DS($auth3DS);
+    return $auth3DS;
 }
-//
-//function testGetXMLAuth3DSStep2():void{
-//    $auth3DS = new Auth3DSStep2RequestDto();
-//    $auth3DS->setShopId("12345678");
-//    $auth3DS->setOperatorId("operator");
-//    $auth3DS->setOriginalRefReqNum("dgbsdhgfiudf");
-//    $auth3DS->setPaRes("Ssdfljlkj45098asdkgr09adsflkj9v26sfaheu73tags52gq7asgdhsdhvadghasags");
-////    $auth3DS->setAcquirer("acuqirer");
-//
-//    echo $auth3DS->getXML();
-//}
